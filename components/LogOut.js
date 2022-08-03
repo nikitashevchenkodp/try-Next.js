69f8c273f1ece7953bf0a7cbe9557be936dbc065
Nikita Shevchenko <nrcnekita9@gmail.com>
2022-08-03 08:12:09 +0200

    End of edit conflict file

--- components/LogOut.js
+++ components/LogOut.js
@@ -3,12 +3,8 @@ import React from 'react'
 const LogOut = () => {
   return (
     <div>
-<<<<<<< HEAD
-
-=======
       LogOut
       <input/>
->>>>>>> 7406947 (Revert "Revert "Add input to Logout"")
     </div>
   )
 }
